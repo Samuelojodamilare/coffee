1304c5ba9d7b7ef6051ec38978fafb74ba208803
Samuelojodamilare <samueldamilare444@gmail.com>
2023-05-29 16:13:13 +0100

    added the calculatorapp project file to the repo

--- practice.html/main.js
+++ practice.html/main.js
@@ -1,78 +1,27 @@
-function outer() {
-  let name = "outer";
-  let str = inner();
-  return str;
-}
-
-function inner() {
-  let name = "inner";
-  return "Hello !";
-}
-
-
-async function getWeather(city, info) {
+const api_urls = 'https://nominatim.openstreetmap.org/search?country=Norway&city=Oslo&format=json';
+const api_url = 'https://services.swpc.noaa.gov/json/ovation_aurora_latest.json';
+async function getForecastByPosition({lon, lat}) {
   try {
-    if (!Array.isArray(city)) {
-      if (info === "all" || typeof info === "undefined") {
-        const response = await fetch(`http://localhost:3000/weather?city=${city}`);
-        const data = await response.json();
-        checkForErrors(data);
-        console.log(data);
-      } else {
-        const validInfoType = ["wind", "clouds", "temp", "precipitation"];
-        if (validInfoType.includes(info)) {
-          const response = await fetch(`http://localhost:3000/weather?city=${city}&type=${info}`);
-          const data = await response.json();
-          checkForErrors(data);
-          console.log(data);
-        } else {
-          console.log(`invalid info parameters: ${info}`)
-        }
-      }
-    } else {
-      for (const cityItem of city) {
-        if (!Array.isArray(city)) {
-          if (info === "all" || typeof info === "undefined") {
-            const response = await fetch(`http://localhost:3000/weather?city=${cityItem}`);
-            const data = await response.json();
-            checkForErrors(data);
-            console.log(data);
-          } else {
-            const validInfoType = ["wind", "clouds", "temp", "precipitation"];
-            if (validInfoType.includes(info)) {
-              const response = await fetch(`http://localhost:3000/weather?city=${cityItem}&type=${info}`);
-              const data = await response.json();
-              checkForErrors(data);
-              console.log(data);
-            } else {
-              console.log(`invalid info parameters: ${info}`)
-            }
-          }
-        }
-      }
-    }
+    const api_url = 'https://services.swpc.noaa.gov/json/ovation_aurora_latest.json';
+    const response = await fetch(api_url)// + `lon=${lon}&lat=${lat}`);
+    const data = await response.json();
+    const [lonIndex, latIndex, auroraIndex] = JSON.parse(data['Data Format'].replace('[','[').replace(']', ']').replace(/Latitude/g, `"lat"`).replace(/Longitude/g, `"lon"`));
+    const {coordinates} = data;
+    const auroraData = coordinates.find(coordinate => coordinate[lonIndex] === lon && coordinate[latIndex] === lat)[auroraIndex];
+    const aurora = auroraData[auroraIndex];
+    console.log(`aurora value: ${aurora}`);
+    return aurora;
   } catch (error) {
-    console.error(error);
-  }
-  function checkForErrors(data) {
-    if (data.weather.wind.speed > 15) {
-      console.warn("WARNING! Wind speed over 15 m/s");
-    }
-    if (data.weather.temp < -20) {
-      console.warn("WARNING! temperature below -20 degrees")
-    }
-  }
+    console.error(`error fetching aurora forecast data: ${error}`) 
+  }  
 }
+getForecastByPosition({lon: 0, lat: -90});
 
 
-/* create a complete responsive coffee shop front-end website design template using html css 
-vanilla javascript the main feature of this website are:
-✅ responsive header with toggle menu effect using vanilla javascript.
-✅ responsive home section using css flexbox.
-✅ responsive about section using css flexbox.
-✅ responsive facility section using css grid.
-✅ responsive menu section using css grid.
-✅ responsive gallery section using css grid.
-✅ responsive team section using css grid.
-✅ responsive contact section using css flexbox.
-✅ responsive footer section using css grid */
+
+async function getForecastByAddress(){
+  const response = await fetch(api_url);
+  const data = await response.json()
+  console.log(data)
+}
+getForecastByAddress();
